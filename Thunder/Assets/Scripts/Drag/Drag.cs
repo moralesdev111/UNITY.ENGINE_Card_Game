c3445288098e0d203428c7a65a0e5f331b1b9e50
Guillermo Mora <moralesdev111@gmail.com>
2024-02-04 23:37:43 +0100

    Advanced Dragging Completed

--- Thunder/Assets/Scripts/Drag/Drag.cs
+++ Thunder/Assets/Scripts/Drag/Drag.cs
@@ -2,29 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    public Transform parentToReturnTo = null;
-
+    public Transform defaultParent = null;
+    public Transform placeHolderParent = null;
+    GameObject placeHolder = null;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        parentToReturnTo = this.transform.parent;
+        placeHolder = new GameObject();
+        placeHolder.transform.SetParent(this.transform.parent);
+        LayoutElement le = placeHolder.AddComponent<LayoutElement>();
+        le.preferredWidth = 125;
+        le.preferredHeight = 200;
+        le.flexibleHeight = 0;
+        le.flexibleWidth = 0;
+
+        placeHolder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+
+        defaultParent = this.transform.parent;
+        placeHolderParent = defaultParent;
         this.transform.SetParent(this.transform.root);
+        
         GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         this.transform.position = eventData.position;
+
+        if(placeHolder.transform.parent != placeHolderParent)
+            placeHolder.transform.SetParent(placeHolderParent);
+
+        int newSiblingIndex = placeHolderParent.childCount;
+
+        for(int i = 0; i < placeHolderParent.childCount; i++)
+        {
+            if(this.transform.position.x < placeHolderParent.GetChild(i).position.x)
+            {
+                newSiblingIndex = i;
+                if(placeHolder.transform.GetSiblingIndex() < newSiblingIndex)
+                {
+                    newSiblingIndex--;
+                }
+                break;
+            }
+        }
+        placeHolder.transform.SetSiblingIndex(newSiblingIndex);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-       
-        this.transform.SetParent(parentToReturnTo);
+        this.transform.SetParent(defaultParent);
+        this.transform.SetSiblingIndex(placeHolder.transform.GetSiblingIndex());
         GetComponent<CanvasGroup>().blocksRaycasts = true;
-        
+
+        Destroy(placeHolder);
     }
 }
---
+++ Thunder/Assets/Scripts/Drag/DropZone.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+     [SerializeField] Hand hand;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(eventData.pointerDrag == null) return;
+
+        Drag drag = eventData.pointerDrag.GetComponent<Drag>();
+        if(drag !=null)
+        {
+            drag.placeHolderParent = this.transform;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if(eventData.pointerDrag == null) return;
+
+        Drag drag = eventData.pointerDrag.GetComponent<Drag>();
+        if(drag !=null && drag.placeHolderParent == this.transform)
+        {
+            drag.placeHolderParent = drag.defaultParent;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+{
+    Drag drag = eventData.pointerDrag.GetComponent<Drag>();
+    if (drag != null)
+    {
+        drag.defaultParent = this.transform;
+
+        if (hand != null)
+        {
+            if (drag.placeHolderParent != this.transform)
+            {
+                // Card left the hand and didn't return
+                hand.Container.Remove(drag.GetComponent<CardUI>().card);
+            }
+            else if (this.gameObject.CompareTag("Battlefield"))
+            {
+                // Card moved from the battlefield to the hand
+                hand.Container.Remove(drag.GetComponent<CardUI>().card);
+            }
+            else if(this.gameObject.CompareTag("Hand")){
+                hand.Container.Add(drag.GetComponent<CardUI>().card);
+            }
+        }
+    }
+
+
+}
+
+}
