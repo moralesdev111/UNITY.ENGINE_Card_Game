f8fc85eee7768e2a4bbadb2dba4c025e2b65f72d
Guillermo Mora <moralesdev111@gmail.com>
2024-02-05 00:56:27 +0100

    Refactor && UpToManaCosts

--- Thunder/Assets/Scripts/Drag/Drag.cs
+++ Thunder/Assets/Scripts/Drag/Drag.cs
@@ -2,63 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
+
 
 public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    [SerializeField] CreatePlaceHolder createPlaceHolder;
+    [SerializeField] SetSiblingIndex setSiblingIndex;
     public Transform defaultParent = null;
     public Transform placeHolderParent = null;
-    GameObject placeHolder = null;
+   
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        placeHolder = new GameObject();
-        placeHolder.transform.SetParent(this.transform.parent);
-        LayoutElement le = placeHolder.AddComponent<LayoutElement>();
-        le.preferredWidth = 125;
-        le.preferredHeight = 200;
-        le.flexibleHeight = 0;
-        le.flexibleWidth = 0;
+        createPlaceHolder.CreatePlaceHolderObject();
 
-        placeHolder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+        createPlaceHolder.placeHolder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
 
         defaultParent = this.transform.parent;
         placeHolderParent = defaultParent;
         this.transform.SetParent(this.transform.root);
-        
+
         GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
+
+    
 
     public void OnDrag(PointerEventData eventData)
     {
         this.transform.position = eventData.position;
 
-        if(placeHolder.transform.parent != placeHolderParent)
-            placeHolder.transform.SetParent(placeHolderParent);
+        if (createPlaceHolder.placeHolder.transform.parent != placeHolderParent)
+            createPlaceHolder.placeHolder.transform.SetParent(placeHolderParent);
 
-        int newSiblingIndex = placeHolderParent.childCount;
-
-        for(int i = 0; i < placeHolderParent.childCount; i++)
-        {
-            if(this.transform.position.x < placeHolderParent.GetChild(i).position.x)
-            {
-                newSiblingIndex = i;
-                if(placeHolder.transform.GetSiblingIndex() < newSiblingIndex)
-                {
-                    newSiblingIndex--;
-                }
-                break;
-            }
-        }
-        placeHolder.transform.SetSiblingIndex(newSiblingIndex);
+        setSiblingIndex.SetSiblingIndexOnRuntime();
     }
+
+    
 
     public void OnEndDrag(PointerEventData eventData)
     {
         this.transform.SetParent(defaultParent);
-        this.transform.SetSiblingIndex(placeHolder.transform.GetSiblingIndex());
+        this.transform.SetSiblingIndex(createPlaceHolder.placeHolder.transform.GetSiblingIndex());
         GetComponent<CanvasGroup>().blocksRaycasts = true;
 
-        Destroy(placeHolder);
+        Destroy(createPlaceHolder.placeHolder);
     }
 }
---
+++ Thunder/Assets/Scripts/Drag/SetSiblingIndex.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSiblingIndex : MonoBehaviour
+{
+    [SerializeField] Drag drag;
+    [SerializeField] CreatePlaceHolder createPlaceHolder;
+
+    public void SetSiblingIndexOnRuntime()
+    {
+        int newSiblingIndex = drag.placeHolderParent.childCount;
+
+        for (int i = 0; i < drag.placeHolderParent.childCount; i++)
+        {
+            if (this.transform.position.x < drag.placeHolderParent.GetChild(i).position.x)
+            {
+                newSiblingIndex = i;
+                if (createPlaceHolder.placeHolder.transform.GetSiblingIndex() < newSiblingIndex)
+                {
+                    newSiblingIndex--;
+                }
+                break;            }
+        }
+        createPlaceHolder.placeHolder.transform.SetSiblingIndex(newSiblingIndex);
+    }
+}
